461593062bd3aae1f124b1626770ecf65130f063
negativeh0ll0w <sarahking573@gmail.com>
2022-08-10 17:29:09 -0400

    fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,58 +1,70 @@
-const CACHE_NAME = 'budget-tracker';
-const DATA_CACHE_NAME = 'budget-data';
+// global constants 
+const APP_PREFIX = 'budget-tracker-';     
+const VERSION = 'v1';
+const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    '/index.html',
-    '/css/styles.css',
-    '/js/index.js',
-    '/icons/icon-192x192.png'
+  '/',
+  '/index.html',
+  '/js/index.js',
+  '/js/idb.js',
+  '/manifest.json',
+  '/css/styles.css',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'  
 ];
 
-// install service worker
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('Service worker installed');
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-    self.skipWaiting();
-});
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
 
-// delete outdated caches
+    })
+  )
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+
+// Delete outdated caches
 self.addEventListener('activate', function(e) {
-    e.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log('Removing old cache data', key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
-})
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      // only return caches for this app
+      let keepCache = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      keepCache.push(CACHE_NAME);
 
-// use cache
-self.addEventListener('fetch', function(e) {
-    if (e.request.url.includes('/api/')) {
-        e.respondWith(
-            caches.open(DATA_CACHE_NAME)
-                .then(cache => {
-                    return fetch(e.request)
-                        .then(response => {
-                            if (response.status === 200) {
-                                cache.put(e.request.url, response.clone());
-                            }
-                            return response;
-                        }).catch(err => {
-                            return cache.match(e.request);
-                        })
-                })
-                .catch(err => console.log(err))
-        );
-        return;
-    }
-})
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
